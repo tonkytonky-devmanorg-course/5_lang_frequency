aed61e9200adf225f5b16ece9c96bd0d62980982
tonkytonky <as@asamsonov.me>
2017-08-12 14:17:47 +0300

    Add main functionality

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,36 @@
+import sys
+import re
 
 
-def load_data(filepath):
-    pass
+SPLIT_LIST = ['\.', ',', ':', ';', '\(', '\)', '!', '\?', '"', '«', '»']
+SPLIT_PATTERN = '\s|' + '|'.join('\s?{}\s?'.format(punctuation) for punctuation in SPLIT_LIST)
 
 
-def get_most_frequent_words(text):
-    pass
+def main():
+    try:
+        get_most_frequent_words(sys.argv[1])
+    except IndexError:
+        print(
+            'Please, call the script with a path to a TXT file. '
+            'You need to specify the path as the first script argument.'
+        )
+
+
+def get_most_frequent_words(filepath):
+    frequency_dict = {}
+    for words in load_words(filepath):
+        for word in words:
+            frequency_dict[word] = frequency_dict.get(word, 0) + 1
+
+    for word in sorted(frequency_dict.items(), key=lambda item: item[1], reverse=True)[:10]:
+        print(word[0])
+
+
+def load_words(filepath):
+    with open(filepath, 'r', encoding='utf-8') as input_file:
+        for line in input_file:
+            yield [word for word in re.split(SPLIT_PATTERN, line.strip().lower()) if word]
 
 
 if __name__ == '__main__':
-    pass
+    main()
